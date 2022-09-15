195a87e5c7322dc728e67f35cc21d4cea8e6ad82
CHOUSHIN CHOU <smzhg0524@gmail.com>
2022-09-15 16:45:38 +0900

    little fix

--- YUB/Control.h
+++ YUB/Control.h
@@ -13,35 +13,35 @@ public:
     void MainControl(Sbus* sbus);
     void MotorControl();
 private:
-    int servoOutputPin[SERVO_INDEX];
-    int ESCOutputPin;
-    int servoOutput[SERVO_INDEX];
     Servo servoObj[SERVO_INDEX];
     Servo ESCObj;
+    uint8_t servoOutputPin[SERVO_INDEX];
+    uint8_t ESCOutputPin;
+    uint16_t servoOutput[SERVO_INDEX];
 private:
     // int16_t ---> 16bits  int
     // left aileron control range: 0 - 180, flat: 90 CH1
-    int16_t leftAileronAngle;
+    uint16_t leftAileronAngle;
     
     // right aileron control range: 0 - 180, flat: 90 CH1
-    int16_t rightAileronAngle;
+    uint16_t rightAileronAngle;
 
     // elevator control range: 0 - 180, flat: 90 CH2
-    int16_t elevatorAngle;
+    uint16_t elevatorAngle;
     
     // left throttle control range: 0 - 100, max power: 100 CH3
-    int16_t leftThrottle;
-    int16_t thrust;
+    uint16_t leftThrottle;
+    uint16_t thrust;
     
     // side-force Plate control !!not used!! CH4
-    int16_t sideForcePlate;
+    uint16_t sideForcePlate;
     
     // rudder control range: 0 - 180, flat: 90 CH5
-    int16_t rudderAngle;
+    uint16_t rudderAngle;
 
     // right throttle control range: 0 - 100, max power: 100 CH6
     // not be used when single motor
-    int16_t rightThrottleRatio;
+    uint16_t rightThrottleRatio;
 };
 
 #endif
