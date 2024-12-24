10e0350906191f336f88f12f5147f484cf6e34c2
CHOSHIN CHO <smzhg0524@gmail.com>
2024-12-24 15:06:46 +0900

    para change

--- YUB/Control.cpp
+++ YUB/Control.cpp
@@ -169,7 +169,7 @@ void Control::MainControl(Sbus *sbus, Sensor *sensor)
 
     if (IfRosTrue)
     {
-        rudderAngle = 122;
+        rudderAngle = 124;
         sideForcePlate = rudderAngle;
     }
     
@@ -254,7 +254,7 @@ void Control::MotorControl(Sbus *sbus, Barometer *brm, float altitude)
 
         //fixedPressure = fixedPressure - (-1.38 * 800 * pow(10, -4) + 4.4 * pow(800, 2) * pow(10, -7) - 1.3 * pow(800, 3) * pow(10, -10));
         //thrust[0] = thrust[0] - THU_KP * (altitude + 6.0f * pressure_diff - altitudeRef) + THU_RUD_KP * abs(90 - rudderAngle);
-        thrust[0] = thrust[0] + THU_KP * (fixedPressure - (takeoffPressure + 0.08 - 0.27)) + THU_RUD_KP * abs(90 - rudderAngle);
+        thrust[0] = thrust[0] + THU_KP * (fixedPressure - (takeoffPressure + 0.08 - 0.17)) + THU_RUD_KP * abs(90 - rudderAngle);
         if (thrust[0] > 1150)
         {
             thrust[0] = 1150;
